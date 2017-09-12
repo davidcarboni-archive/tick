bf4a543fde630f275743607ddc771fa7a8b51eb8
David Carboni <david.carboni@ros.gov.uk>
2017-09-12 14:32:57 +0100

    Caching counters and renamed TELEGRAF to STATSD.

--- app/app.py
+++ app/app.py
@@ -11,14 +11,15 @@ from flask import Flask, request, redirect, render_template, jsonify
 
 # Config
 
-TELEGRAF = os.getenv("TELEGRAF") or "localhost"
-statsd.Connection.set_defaults(host=TELEGRAF, port=8125)
+STATSD = os.getenv("STATSD") or "localhost"
+statsd.Connection.set_defaults(host=STATSD, port=8125)
 
 # Logging
 
 logging.getLogger().setLevel(logging.DEBUG)
 logging.getLogger("b3").setLevel(logging.INFO)
 log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 
 # App
@@ -26,23 +27,36 @@ log = logging.getLogger(__name__)
 app = Flask("tick", static_folder='static', static_url_path='')
 
 
+status_counters = {}
+request_frequency = statsd.Counter('request_frequency')
+
+
+def status_code_counter(response):
+    global status_counters
+    status_code = response.status_code
+    if status_code not in status_counters:
+        # We explicitly ignore race conditions on the basis this is 'good enough':
+        status_counters[status_code] = statsd.Counter('status_' + str(response.status_code))
+    return status_counters[status_code]
+
+
 def count_request_frequency():
     log.info("Incrementing request_frequency")
-    request_frequency = statsd.Counter('request_frequency')
+    global request_frequency
     request_frequency += 1
 
 
-def count_errors(response):
-    log.info("Incrementing response_code_frequency: " + str(response.status_code))
-    response_code_frequency = statsd.Counter('status_' + str(response.status_code))
-    response_code_frequency += 1
+def count_response_codes(response):
+    log.info("Incrementing status code frequency: " + str(response.status_code))
+    status_code_frequency = status_code_counter(response)
+    status_code_frequency += 1
     return response
 
 
 app.before_request(b3.start_span)
 app.before_request(count_request_frequency)
-app.after_request(count_errors)
 app.after_request(b3.end_span)
+app.after_request(count_response_codes)
 
 
 @app.route('/')
